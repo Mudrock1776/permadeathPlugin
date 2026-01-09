791561326086f2dfd989f7bcb03dd50cdbfbc99c
Mudrock1776 <144382700+Mudrock1776@users.noreply.github.com>
2026-01-09 10:18:58 -0500

    feat: added tpa commands (#10)

--- src/main/java/mudrock/commands/utilities/tpaCommand.java
+++ src/main/java/mudrock/commands/utilities/tpaCommand.java
@@ -1,5 +1,38 @@
 package mudrock.commands.utilities;
 
-public class tpaCommand {
+import org.bukkit.ChatColor;
+import org.bukkit.NamespacedKey;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.persistence.PersistentDataType;
+
+import mudrock.permadeathPlugin;
+
+public class tpaCommand implements CommandExecutor{
+    private final permadeathPlugin plugin;
+    public tpaCommand(permadeathPlugin plugin){
+        this.plugin = plugin;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (sender instanceof Player) {
+            Player player = (Player) sender;
+            if (args.length == 0) {
+                player.sendMessage(ChatColor.RED+"Required Player Name");
+            } else {
+                Player target = plugin.getServer().getPlayer(args[0]);
+                if (target == null) {
+                    player.sendMessage(ChatColor.RED+"Unknown Player: "+args[0]);
+                } else {
+                    target.getPersistentDataContainer().set(new NamespacedKey(plugin, "tprequest"), PersistentDataType.STRING, player.getName());
+                    target.sendMessage(ChatColor.WHITE+player.getName()+ChatColor.GOLD+" would like to teleport to you /tpaccept to accept, /tpdeny to deny");
+                }
+            }
+        }
+        return true;
+    }
 
 }
